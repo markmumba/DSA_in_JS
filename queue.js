ef7386c35cdce97ac4332620c1bf85dbbc5eeebd
markmumba <markmumba01@gmail.com>
2022-10-29 13:46:56 +0300

    created a queue

--- queue.js
+++ queue.js
@@ -1,4 +1,40 @@
-class Queue {
+// class Queue {
+//     constructor() {
+//         this.collections = [];
+
+//         this.print = function () {
+//             return this.collections;
+
+//         }
+//         this.enqueue = function () {
+//             for (let i = 0; i < arguments.length; i++) {
+//                 this.collections.push(arguments[i]);
+//             }
+//             return this.collections;
+//         }
+//         this.dequeue = function () {
+
+//             return this.collections.shift();
+//         }
+//         this.size = function () {
+//             let size = this.collections.length;
+//             return size
+//         }
+//         this.isEmpty = function () {
+//             return (this.collections.length === 0);
+//         }
+//     }
+
+// };
+
+
+// let newQueue = new Queue;
+// console.log (newQueue.enqueue(1,2,3,4,5,6,7,8,9,10,11,12));
+// console.log (newQueue.dequeue());
+// console.log (newQueue.size());
+
+
+class PriorityQueue {
     constructor() {
         this.collections = [];
 
@@ -6,14 +42,26 @@ class Queue {
             return this.collections;
 
         }
-        this.enqueue = function () {
-            for (let i = 0; i < arguments.length; i++) {
-                this.collections.push(arguments[i]);
+        this.enqueue = function (value) {
+            if (this.isEmpty()) {
+                this.collections.push(value);
+            } else {
+                let added = false;
+                for (let i = 0; i < this.collections.length; i++) {
+                    if (value[1] < this.collections[i][1]) {
+                        this.collections.splice(i, 0, value);
+                        added = true;
+                        break;
+                    }
+                }
+                if (!added) {
+                    this.collections.push(value);
+                }
             }
-            return this.collections;
+
         }
         this.dequeue = function () {
-            
+
             return this.collections.shift();
         }
         this.size = function () {
@@ -24,11 +72,13 @@ class Queue {
             return (this.collections.length === 0);
         }
     }
+}
 
-};
+let newQueue = new PriorityQueue();
 
-
-let newQueue = new Queue;
-console.log (newQueue.enqueue(1,2,3,4,5,6,7,8,9,10,11,12));
-console.log (newQueue.dequeue());
-console.log (newQueue.size());
+newQueue.enqueue(['George', 4]);
+newQueue.enqueue(['panther', 9]);
+newQueue.enqueue(['elon', 3]);
+newQueue.enqueue(['Markian', 1]);
+newQueue.enqueue(['Lionel', 8]);
+console.log(newQueue.print());
